ddf807eb153d27260514a23662bd58b3723595d5
Vakor <myksv200@gmail.com>
2022-01-18 01:36:20 +0200

    18.01 Console application

---
+++ Vakor.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vakor.ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
